d6e5a4e75d30864b395b54f97ddc3428fdaab32a
Kingsley Eze <firebox@ikingsley.com>
2020-12-19 11:49:54 +0100

    Update record search API

--- src/routes/record/controllers/record.js
+++ src/routes/record/controllers/record.js
@@ -1,6 +1,43 @@
+import Record from '../../../models/Record'
+
 export default async ({ bodymen: { body } }, res, next) => {
   try {
-    return res.json({ message: 'Hello World!' })
+    const { startDate, endDate, minCount, maxCount } = body
+    console.log(startDate, endDate, minCount, maxCount)
+
+    Record.aggregate([
+      {
+        $project: {
+          _id: false,
+          key: 1,
+          createdAt: 1,
+          totalCount: {
+            $reduce: {
+              input: '$counts',
+              initialValue: 0,
+              in: { $add: ['$$value', '$$this'] }
+            }
+          }
+        }
+      },
+      {
+        $match: {
+          $and: [
+            { createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) } },
+            { totalCount: { $gt: minCount, $lt: maxCount } }
+          ]
+        }
+      }
+    ])
+      .exec()
+      .then(data => {
+        res.status(200).json({
+          code: 0,
+          msg: 'Success',
+          records: data
+        })
+      })
+      .catch(next)
   } catch (err) {
     return next(err)
   }
